5d05dadec243028440170eed55e013c48578a9a0
HoangAnhNguyen0710 <anhnh1@hblab.vn>
2022-09-17 00:30:58 +0700

    fix deploy bug on heroku

--- src/modules/review/review.module.ts
+++ src/modules/review/review.module.ts
@@ -1,12 +1,13 @@
 /* eslint-disable prettier/prettier */
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from '../user/user.entity';
 import { ReviewController } from './review.controller';
 import { Review } from './review.entity';
 import { ReviewService } from './review.service';
 
 @Module({
-    imports: [TypeOrmModule.forFeature([Review])],
+    imports: [TypeOrmModule.forFeature([Review, User])],
     controllers:[ReviewController],
     providers:[ReviewService],
 })
